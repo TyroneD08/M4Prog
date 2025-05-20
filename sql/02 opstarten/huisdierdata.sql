3fd1d04ad1b82ea781085a4ed196067ca610148f
Tyrone <112290278+TyroneD08@users.noreply.github.com>
2025-05-20 10:53:50 +0200

    update

---
+++ sql/02 opstarten/huisdierdata.sql
@@ -0,0 +1,15 @@
+INSERT INTO `huisdieren` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`)
+ VALUES (NULL, 'Jaguar', 'Tamari Simms', 'Roofdier', '7', '20-1-20');
+
+INSERT INTO `huisdieren` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`)
+ VALUES
+  (NULL, 'Tijger', 'Simms Tamari', 'Roofdier', '6', '20-2-20'),
+  (NULL, 'Vis', 'Tamari', 'zeedier', '4', '21-2-20');
+
+
+  INSERT INTO `huisdieren` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`)
+ VALUES
+  (NULL, 'Giraffe', 'beni', 'Zoogdier', '8', '10-2-20'),
+  (NULL, 'Leeuw', 'sam', 'Roofdier', '8', '10-2-20'),
+  (NULL, 'Bever', 'rick', 'Roofdier', '8', '10-2-20'),
+  (NULL, 'Olifant', 'benflii', 'zoogdier', '41', '21-8-20');
