e0c019b171fd0a31ea0b8eacde19cf0b885adfcf
Tyrone <112290278+TyroneD08@users.noreply.github.com>
2025-05-23 10:30:09 +0200

    04upd

---
+++ sql/04 opstarten/where.sql
@@ -0,0 +1,14 @@
+select * from huisdieren where leeftijd <= 4;
+
+select * from huisdieren where leeftijd >= 5;
+
+select * from huisdieren where leeftijd = 4;
+
+select naam, leeftijd from huisdieren where naam = 'Jaguar';
+
+select naam, leeftijd from huisdieren where naam = 'Vis';
+
+select * from huisdieren where typedier = 'Roofdier';
+
+select * from huisdieren where typedier = 'zeedier';
+
